87f7a6c31518f88f6b546bb060b574bec630ab86
Olivier-Kango <olivierkango@gmail.com>
2022-10-18 18:46:36 -0700

    Import function recentSores

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import _, { add } from 'lodash';
 import './style.css';
-import {
-  toDoList,
-} from './modules/addremove.js';
+import recentSores from './modules/setup.js';
 /* eslint-enable no-unused-vars */
+
+recentSores();
