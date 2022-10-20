655755cd81ba0438abbda92ded70467949785f0d
Olivier-Kango <olivierkango@gmail.com>
2022-10-20 14:54:33 -0700

    Remove linter errors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-/* eslint-disable no-unused-vars */
-import _, { add } from 'lodash';
 import './style.css';
 import {
   gameScore, button, addScore, submitButton,
 } from './modules/hit-api.js';
-/* eslint-enable no-unused-vars */
+
 button.addEventListener('click', gameScore, { once: true });
 submitButton.addEventListener('click', addScore);
