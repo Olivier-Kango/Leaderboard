c488d999f64ffa6f6162e4e6764f71a4389a58f0
Olivier-Kango <olivierkango@gmail.com>
2022-10-20 13:42:48 -0700

    Implement the code for the submit button

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 /* eslint-disable no-unused-vars */
 import _, { add } from 'lodash';
 import './style.css';
-import { gameScore, button } from './modules/hit-api.js';
+import {
+  gameScore, button, addScore, submitButton,
+} from './modules/hit-api.js';
 /* eslint-enable no-unused-vars */
 button.addEventListener('click', gameScore, { once: true });
+submitButton.addEventListener('click', addScore);
--- src/modules/hit-api.js
+++ src/modules/hit-api.js
@@ -1,16 +1,31 @@
 import setupProject, { recentSores } from './setup.js';
 
 export const button = document.getElementById('button');
+export const submitButton = document.getElementById('btn');
+
+export const addScore = async () => {
+  const yourName = document.querySelector('#your-name');
+  const yourScore = document.querySelector('#your-score');
+  const baseUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
+  const url = `${baseUrl}games/`;
+  const gameID = 'kangoishara';
+  const response = await fetch(`${url}${gameID}/scores/`, {
+    method: 'POST',
+    headers: { 'Content-type': 'application/json' },
+    body: JSON.stringify({ user: yourName.value, score: yourScore.value }),
+  });
+  const res = await response.json();
+  return res;
+};
 
 export const gameScore = async () => {
-  const requestString = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Zl4d7IVkemOTTVg2fUdz/scores/';
+  const requestString = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/kangoishara/scores/';
 
   const data = await fetch(requestString);
 
   const response = await data.json();
   const scoreGame = response.result;
-  const newGame = scoreGame.slice(0, 8);
-  newGame.forEach((element) => {
+  scoreGame.forEach((element) => {
     recentSores.push(element);
   });
   setupProject();
