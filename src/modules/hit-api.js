6cb4910319242b7a2cef18d76ec1c50bfea573a6
Olivier-Kango <olivierkango@gmail.com>
2022-10-20 14:24:43 -0700

    Create a new branch development

---
+++ src/modules/hit-api.js
@@ -0,0 +1,33 @@
+import setupProject, { recentSores } from './setup.js';
+
+export const button = document.getElementById('button');
+export const submitButton = document.getElementById('btn');
+
+export const addScore = async () => {
+  const yourName = document.querySelector('#your-name');
+  const yourScore = document.querySelector('#your-score');
+  const baseUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
+  const url = `${baseUrl}games/`;
+  const gameID = 'kangoishara';
+  const response = await fetch(`${url}${gameID}/scores/`, {
+    method: 'POST',
+    headers: { 'Content-type': 'application/json' },
+    body: JSON.stringify({ user: yourName.value, score: yourScore.value }),
+  });
+  const res = await response.json();
+  window.location.reload();
+  return res;
+};
+
+export const gameScore = async () => {
+  const requestString = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/kangoishara/scores/';
+
+  const data = await fetch(requestString);
+
+  const response = await data.json();
+  const scoreGame = response.result;
+  scoreGame.forEach((element) => {
+    recentSores.push(element);
+  });
+  setupProject();
+};
